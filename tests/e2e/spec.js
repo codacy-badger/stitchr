a4df8c8ae3ba5dbd8afefb9aba01188388883c48
Unknown <jamin546@gmail.com>
2018-11-04 21:54:23 +0000

    Set up project

---
+++ tests/e2e/spec.js
@@ -0,0 +1,40 @@
+const Application = require('spectron').Application
+const assert = require('assert')
+const path = require('path')
+
+let electronPath = path.join(__dirname, '..', '..', 'out', `stitchr-${process.platform}-${process.arch}`, 'stitchr')
+
+if (process.platform === 'win32') {
+  electronPath += '.cmd'
+}
+
+describe('Application launch', function () {
+  this.timeout(10000)
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '..', '..')],
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+    return null
+  })
+
+  it('shows an initial window', function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1)
+    })
+  })
+
+  it('has the correct title', function () {
+    return this.app.client.getTitle().then(function (title) {
+      assert.equal(title, 'Stitchr')
+    })
+  })
+})
