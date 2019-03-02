6388db71f3ace41d8688585143227268ef167fc3
Benjamin Wibrew <benjamin.wibrew@gmail.com>
2019-03-02 18:36:42 +0000

    [WIP] Add navbar component

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="root">
+    <navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+}
+</script>
---
+++ src/renderer/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar__nav-items">
+      <li
+        v-for="navbarItem in navbarItems"
+        :key="navbarItem.id"
+        class="navbar__nav-item"
+      >
+        {{ navbarItem.name }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      navbarItems: [
+        {
+          name: 'Home',
+          link: '#',
+        },
+        {
+          name: 'Create',
+          link: '#',
+        },
+        {
+          name: 'Edit',
+          link: '#',
+        },
+        {
+          name: 'Save',
+          link: '#',
+        },
+      ],
+    }
+  },
+}
+</script>
